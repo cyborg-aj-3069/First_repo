27cdd78e79ba4ed3128ddd8881d2565c8ebefc75
Abhishek Jha <75085546+cyborg-aj-3069@users.noreply.github.com>
2021-01-29 22:55:41 +0530

    To check Armstrong no.

---
+++ Armstrong.java
@@ -0,0 +1,21 @@
+class Assignment1Q1 {
+    public static void main(String[] args) {
+	 
+	 int num = 371, originalNumber, remainder, result = 0;
+
+        originalNumber = num;
+
+        while (originalNumber != 0)
+        {
+            remainder = originalNumber % 10;
+            result += Math.pow(remainder, 3);
+            originalNumber /= 10;
+        }
+	if(result == num)
+	System.out.println(num + " is an Armstrong number.");
+    else
+    System.out.println(num + " is not an Armstrong number.");
+	
+	}
+}
+
